450f5882fb67028db509ef6b03b83db4ff5a0389
Penn Junior <pennjunior680@gmail.com>
2023-11-28 11:48:17 -0800

    Working task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-letters = ''.join(
-    f"{chr(char)}" for char in range(ord('a'), ord('z') + 1)
-)
-print(letters, end='')
+for char in range(ord('a'), ord('z') + 1):
+    print(chr(char).format(), end='')
