3803e08f4a1ea6ef0a525173260f0d0d299feb13
Penn Junior <pennjunior680@gmail.com>
2023-11-30 07:46:29 -0800

    Working on task 0

--- 0x02-python-import_modules/0-import_add.py
+++ 0x02-python-import_modules/0-import_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from importlib import import_module
+if __name__ == "__main__":
+    from importlib import import_module
 
-module = import_module("0-add")
-add = module.add
+    module = import_module("0-add")
+    add = module.add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
