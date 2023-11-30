ea6b5df5b4c608056ac205e02074d242f102a88a
Penn Junior <pennjunior680@gmail.com>
2023-11-30 09:21:27 -0800

    Working on task 2 corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     if args_count == 0:
         print("0 arguments.")
     else:
-        print(f"{args_count} {'argument' if args_count == 1 else 'arguments'}:")
+        plural = '' if args_count == 1 else 's'
+        print(f"{args_count} argument{plural}:")
         for index, arg in enumerate(argv[1:], start=1):
             print(f"{index}: {arg}")
