a6c04b04ef341ec0152207c6ccdf80b4d09c43b1
Penn Junior <pennjunior680@gmail.com>
2023-12-04 03:13:15 -0800

    Doing some corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-
-    return my_list[idx]
-
-
-my_list = [10, 20, 30, 40, 50]
-result = element_at(my_list, 2)
-print(result)  # Output will be 30
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
