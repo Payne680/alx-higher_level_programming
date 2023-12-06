b367d8b1084e15a846ee92980d96912830c82a8c
Penn Junior <pennjunior680@gmail.com>
2023-12-06 05:55:55 -0800

    Correcting task 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix):
-    result = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            result[i][j] = matrix[i][j] ** 2
+            matrix[i][j] = matrix[i][j] ** 2
 
-            return result
+            return matrix
